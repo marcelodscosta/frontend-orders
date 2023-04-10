71ef5a4ff81ffb4b765ee579dc2978582b8c0123
Marcelo Costa <marcelodscosta@yahoo.com.br>
2023-04-10 12:03:22 -0300

    settings for authentication and routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import SignIn from "./components/SignIn";
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 
+
 const theme = createTheme();
 
 
 export const App = () => {
   return (
     <ThemeProvider theme={theme}>
+
       <SignIn />
+
     </ThemeProvider>
   )
 }
